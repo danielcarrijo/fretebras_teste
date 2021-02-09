6eed71700ab548595414f7ec8dd5896f6a086b11
danielcarrijo <daniel.carrijo@outlook.com>
2021-02-08 21:49:42 -0300

    Seeds para bd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $path = 'bd/dump-pineapple-202102082144.sql';
+        DB::unprepared(file_get_contents($path));
     }
 }
